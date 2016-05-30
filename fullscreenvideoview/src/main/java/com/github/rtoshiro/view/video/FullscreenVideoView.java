193d787d285a9388c0d6574c6283b02b197dcc80
Toshiro Sugii <rtoshiro@printwtf.com>
2016-05-30 12:20:18 -0300

    license added.

--- fullscreenvideoview/src/main/java/com/github/rtoshiro/view/video/FullscreenVideoView.java
+++ fullscreenvideoview/src/main/java/com/github/rtoshiro/view/video/FullscreenVideoView.java
@@ -1,4 +1,18 @@
-
+/**
+ * Copyright (C) 2016 Toshiro Sugii
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.rtoshiro.view.video;
 
 import android.app.Activity;
@@ -45,7 +59,7 @@ public class FullscreenVideoView extends RelativeLayout implements SurfaceHolder
     private final static String TAG = "FullscreenVideoView";
 
     protected Context context;
-    protected Activity activity;
+    protected Activity activity; // Used when orientation changes is not static
 
     protected MediaPlayer mediaPlayer;
     protected SurfaceHolder surfaceHolder;
@@ -57,8 +71,7 @@ public class FullscreenVideoView extends RelativeLayout implements SurfaceHolder
 
     protected View loadingView;
 
-    // Controla o fullscreen
-    protected ViewGroup parentView;
+    protected ViewGroup parentView; // Controls fullscreen container
     protected ViewGroup.LayoutParams currentLayoutParams;
 
     protected boolean fullscreen;
